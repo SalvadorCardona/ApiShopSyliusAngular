4ed34e507631280fc97a47610bb46ca9e1205610
salvador <hebustha@gmail.com>
2020-01-29 21:27:27 +0100

    Add Docker

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
-  apiBaseUrl: 'http://back.local:8000',
-  client_id: '3y4fn3b5iv28owgwo8gwwkc44kkg48g8k0s8480oowc4swsgks',
-  client_secret: '1xsadrxlj58kgo8o0gw40g40wk8gwkk44884ok84okckcsc0kc',
-  production: false,
-  hmr: true,
-  version: '1-dev',
-  supportedLanguages: ['en-US', 'fr-FR'],
-  defaultLanguage: 'en-US'
+   apiBaseUrl: 'http://back.local:8000',
+   client_id: '3y4fn3b5iv28owgwo8gwwkc44kkg48g8k0s8480oowc4swsgks',
+   client_secret: '1xsadrxlj58kgo8o0gw40g40wk8gwkk44884ok84okckcsc0kc',
+   production: false,
+   hmr: true,
+   version: '1-dev',
+   supportedLanguages: ['en-US', 'fr-FR'],
+   defaultLanguage: 'en-US',
 };
